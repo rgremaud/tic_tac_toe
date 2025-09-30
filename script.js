de4a840e584270292fda0cfa30c72f5a0df0efbe
Romain Gremaud <rgremau@gmail.com>
2025-09-30 17:38:39 -0400

    Add initial framework for screen controller

--- script.js
+++ script.js
@@ -90,6 +90,66 @@ function GameInit(playerOne, playerTwo) {
     return {
         playRound,
         getActivePlayer,
-        //getBoard: board.getBoard
+        getBoard: board.getBoard
     }
 }
+
+function ScreenController() {
+    const game = GameInit();
+    // needs to render the current player
+    // 
+    // needs to refresh HTML board after each play
+    // return a render function to be used with GameInit
+}
+
+/*
+Example screen controller from building a house article
+function ScreenController() {
+  const game = GameController();
+  const playerTurnDiv = document.querySelector('.turn');
+  const boardDiv = document.querySelector('.board');
+
+  const updateScreen = () => {
+    // clear the board
+    boardDiv.textContent = "";
+
+    // get the newest version of the board and player turn
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    // Display player's turn
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+
+    // Render board squares
+    board.forEach(row => {
+      row.forEach((cell, index) => {
+        // Anything clickable should be a button!!
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        // Create a data attribute to identify the column
+        // This makes it easier to pass into our `playRound` function 
+        cellButton.dataset.column = index
+        cellButton.textContent = cell.getValue();
+        boardDiv.appendChild(cellButton);
+      })
+    })
+  }
+
+  // Add event listener for the board
+  function clickHandlerBoard(e) {
+    const selectedColumn = e.target.dataset.column;
+    // Make sure I've clicked a column and not the gaps in between
+    if (!selectedColumn) return;
+    
+    game.playRound(selectedColumn);
+    updateScreen();
+  }
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+  // Initial render
+  updateScreen();
+
+  // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+}
+ScreenController();
+*/
